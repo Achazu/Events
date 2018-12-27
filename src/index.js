63c39b6fa819a0f1f10f72ccedbf1f7e6f374943
Luke_t <lukasz.tylke@gmail.com>
2018-12-27 22:06:28 +0100

    basic layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './App';
+import App from './app/layouts/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+let render = () => {
+	ReactDOM.render(<App />, rootEl)
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+if(module.hot) {
+	module.hot.accept('./app/layouts/App', () => {
+		setTimeout(render)
+	})
+}
+
+render();
+
 serviceWorker.unregister();
