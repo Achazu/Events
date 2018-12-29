de544ec88b9eb4952dac52e5bdd03d55aa2e778a
Luke_t <lukasz.tylke@gmail.com>
2018-12-29 15:01:43 +0100

    Redux intro

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducer';
+
+export const configureStore = (preloadedState) => {
+	const middlewares = [];
+	const middlewareEnhancer = applyMiddleware(...middlewares);
+	const storeEnhancers = [middlewareEnhancer];
+	// const composedEnhancer = compose(...storeEnhancers);
+	const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+	const store = createStore(
+		rootReducer,
+		preloadedState,
+		composedEnhancer
+	)
+
+	// enabling hot module replacement for redux as well (it was already done for react in app.js or index.js)
+	if(process.env.NODE_ENV !=='production'){
+		if(module.hot){
+			module.hot.accept('../reducers/rootReducer', () => {
+				const newRootReducer = require('../reducers/rootReducer').default;
+				store.replaceReducer(newRootReducer);
+			})
+		}
+	}
+
+	return store
+}
